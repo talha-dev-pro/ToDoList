5a5ccee6dee3bfe287842bc42ceff96768e9f71f
talha-dev-pro <talha.dev.pro@gmail.com>
2024-05-17 03:36:41 +0500

    get, post, put requests

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,7 +2,15 @@ const joi = require("joi");
 
 const createUserSchema = joi.object().keys({
   userName: joi.string().email().required(),
-  // password: joi.string().min(6).max(18).required,
+  password: joi.string().min(6).max(18).required(),
+});
+const getUserSchema = joi.object().keys({
+  userName: joi.string().email().required(),
+});
+
+const updatePasswordSchema = joi.object().keys({
+  userName: joi.string().email().required(),
+  password: joi.string().min(6).max(18).required(),
 });
 
 const userArray = [];
@@ -11,7 +19,6 @@ module.exports = {
   createUser: async (req, res) => {
     try {
       const validate = await createUserSchema.validateAsync(req.body);
-      // const validate = await createUserSchema.validateAsync(req.query);
       userArray.push(validate);
       console.log(userArray);
 
@@ -25,9 +32,51 @@ module.exports = {
       });
     }
   },
-  getUser: (req, res) => {
-    res.send({
-      message: "Create User",
-    });
+  getUser: async (req, res) => {
+    try {
+      const validate = await getUserSchema.validateAsync(req.query);
+      let getUser = (item) => {
+        if (item.userName === validate.userName) {
+          return item;
+        } else {
+          return "User not found";
+        }
+      };
+      let user1 = userArray.map(getUser);
+      return res.send({
+        message: "Got User",
+        data: user1,
+      });
+    } catch (error) {
+      return res.send({
+        message: error.message,
+      });
+    }
+  },
+  updateUser: async (req, res) => {
+    try {
+      const validate = await updatePasswordSchema.validateAsync(req.body);
+      let getUser = (item) => {
+        if (item.userName === validate.userName) {
+          return item;
+        } else {
+          return "User not found";
+        }
+      };
+      let user1 = userArray.map(getUser);
+      // const { user } = user1[0];
+      console.log(user1);
+      let olduser = user1[0];
+      // user1[0].password = validate.password;
+      return res.send({
+        old: olduser,
+        message: "Password changed",
+        data: user1,
+      });
+    } catch (error) {
+      return res.send({
+        message: error.message,
+      });
+    }
   },
 };
--- routes/userRouter.js
+++ routes/userRouter.js
@@ -1,7 +1,12 @@
 const route = require("express").Router();
-const { getUser, createUser } = require("../controllers/userController");
+const {
+  getUser,
+  createUser,
+  updateUser,
+} = require("../controllers/userController");
 
 route.get("/getUser", getUser);
 route.post("/createUser", createUser);
+route.put("/updateUser", updateUser);
 
 module.exports = route;
